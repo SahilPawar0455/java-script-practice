84d023fa60643c7a8aa54b948c38b1727f77aaef
SAHIL0455 <sahilpawar1004@gmail.com>
2022-10-04 19:06:58 +0530

    ADDED: arithmetic operation

---
+++ IfElseProgram/ArithmetricOperation.js
@@ -0,0 +1,6 @@
+let a = 5, b = 7,c =10;
+console.log("arithmetic operation");
+console.log("a + b * c = " + a + b * c);
+console.log("a % b + c = " + a % b + c);
+console.log("c + a / b = " + c + a / b);
+console.log("a * b + c = " + a * b + c);
