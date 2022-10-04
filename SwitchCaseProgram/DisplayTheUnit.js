352c49941d93855aab00b4fa85c1639068d3534f
SAHIL0455 <sahilpawar1004@gmail.com>
2022-10-04 19:15:03 +0530

    ADDED: Read a Number 1, 10, 100, 1000, etc and display unit

---
+++ SwitchCaseProgram/DisplayTheUnit.js
@@ -0,0 +1,18 @@
+let number = 1000;
+switch(number){
+    case 1:
+        console.log("one");
+        break;
+    case 10:
+        console.log("Ten");
+        break;
+    case 100:
+         console.log("Hundred");
+        break;
+    case 1000:
+        console.log("Thousand");
+        break;
+    case 10000:
+        console.log("Ten Thousand");
+        break;
+}
