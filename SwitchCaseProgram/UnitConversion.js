82655ba3ca071b0c7c7c21da3f2ad877bf74b2ae
SAHIL0455 <sahilpawar1004@gmail.com>
2022-10-04 19:17:29 +0530

    ADDED: Unit Conversion of different Length units

---
+++ SwitchCaseProgram/UnitConversion.js
@@ -0,0 +1,7 @@
+let unit = 12;
+console.log("Unit Conversion");
+console.log("Unit is 12");
+console.log("Unit Conversion feet into inch = "+ unit*12);
+console.log("Unit Conversion inch into feet = "+ unit/12);
+console.log("Unit Conversion meter into feet = "+ unit*3.28084);
+console.log("Unit Conversion meter into feet = "+ unit/3.28084);
